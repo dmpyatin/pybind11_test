a6bd8e7ce93f40006a3b63ea8c783c7a513f98d4
Dmitry Pyatin <dmpyatin@gmail.com>
2022-03-09 17:27:23 +0300

    add substract and multiply functions

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,14 @@ int add(int i, int j) {
     return i + j;
 }
 
+int substact(int i, int j) {
+    return i - j;
+}
+
+int multiply(int i, int j) {
+    return i * j;
+}
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(pybind11_test_dmpytain, m) {
@@ -29,10 +37,16 @@ PYBIND11_MODULE(pybind11_test_dmpytain, m) {
         Some other explanation about the add function.
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+    m.def("subtract", &substact, R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
+    )pbdoc");
+
+    m.def("multiply", &multiply, R"pbdoc(
+        Multiply two numbers
+
+        Some other explanation about the multiply function.
     )pbdoc");
 
 #ifdef VERSION_INFO
