72d69d46709c64fccea6b0a7c936bb64db7b8a3d
Dmitry Pyatin <dmpyatin@gmail.com>
2022-03-10 13:09:37 +0300

    new version fix

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,6 @@
 import pybind11_test_dmpytain as ptd
 
 def test():
-    assert ptd.__version__ == '0.0.3'
+    assert ptd.__version__ == '0.0.4'
     assert ptd.add(1, 2) == 3
     assert ptd.subtract(1, 2) == -1
